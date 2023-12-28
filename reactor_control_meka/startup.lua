f7a74b36142d21d6d03b1ac656c96532d4ba902c
XCO_Yoshi <26392051+XCOYoshi@users.noreply.github.com>
2023-12-28 02:11:47 +0100

    Update startup.lua

--- reactor_control_meka/startup.lua
+++ reactor_control_meka/startup.lua
@@ -3,7 +3,11 @@ function text()
     
     if input == "start" and redstone.getInput("left") == false then
         redstone.setOutput("right", true)
+        local b = fs.open("storage.txt", "w")
+        b.write("true")
+        b.close()
         print("Reactor starting")
+       
     end
     
     if input == "start" and redstone.getInput("left") == true then
@@ -12,6 +16,9 @@ function text()
     
     if input == "stop" then
         redstone.setOutput("right", false)
+        local e = fs.open("storage.txt", "w")
+        e.write("false")
+        e.close()
         print("Reactor stoping")
     end
 end
@@ -21,14 +28,25 @@ function emergency_stop()
 
     if redstone.getInput("left") == true then
         redstone.setOutput("right", false)
+        
         print("Reactor Emergency Stopped")
     end
 
-end
+end 
 
 term.clear()
 term.setCursorPos(1, 1)
 print("reactor-control")
+print("")
+local h = fs.open("storage.txt", "r")
+
+if h.readAll() == "true" then
+    redstone.setOutput("right", true)
+    print("the Reactor is running")
+    h.close()
+else
+    print("the Reactor is NOT running")
+end
 
 while true do
     parallel.waitForAny(text, emergency_stop)
